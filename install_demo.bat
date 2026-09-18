33a3bcd9f3ac5c5de8be45250c145c9cb89fef47
RedoxCd <benjamin.rossetti08@gmail.com>
2026-09-18 12:12:28 +0200

    Delete install_demo.bat

--- install_demo.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Double-clique ce fichier pour installer la démo (voir install_demo.ps1
-REM pour les options -Server / -BackupDir / -Force).
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0install_demo.ps1" %*
-pause
